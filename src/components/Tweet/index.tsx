6c003d66fc4d597bddf385ac0ae625744f307253
Leandross86 <leandrobko@gmail.com>
2020-07-11 15:35:24 -0300

    criado o component tweet

--- src/components/Tweet/index.tsx
+++ src/components/Tweet/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Retweeted,
+  RocketseatIcon,
+  Body,
+  Avatar,
+  Content,
+  Header,
+  Dot,
+  Description,
+  ImageContent,
+  Icons,
+  Status,
+  CommentIcon,
+  RetweetIcon,
+  LikeIcon,
+} from './styles';
 
 const Tweet: React.FC = () => {
-  return <div />;
-}
+  return (
+    <Container>
+      <Retweeted>
+        <RocketseatIcon />
+        Você retweetou
+      </Retweeted>
+
+      <Body>
+        <Avatar />
+        <Content>
+          <Header>
+            <strong>Bko</strong>
+            <span>@bko</span>
+            <Dot />
+            <time>10 de ago</time>
+          </Header>
+
+          <Description>Foguete não tem ré</Description>
+
+          <ImageContent />
+
+          <Icons>
+            <Status>
+              <CommentIcon />
+              56
+            </Status>
+            <Status>
+              <RetweetIcon />
+              23
+            </Status>
+            <Status>
+              <LikeIcon />
+              734
+            </Status>
+          </Icons>
+        </Content>
+      </Body>
+    </Container>
+  );
+};
 
 export default Tweet;
--- src/components/styles/icons.ts
+++ src/components/styles/icons.ts
@@ -10,4 +10,5 @@ export {
 export { Person, Cake, Favorite } from 'styled-icons/material';
 export { ArrowLeft } from 'styled-icons/heroicons-solid';
 export { Chat } from 'styled-icons/heroicons-outline';
-// export { RocketseatIcon as Rocketseat } from '../components/Rocketseat'
+export { AllInclusive } from '@styled-icons/material-rounded/AllInclusive';
+export { RocketseatIcon as Rocketseat } from '../RocketseatIcon';
